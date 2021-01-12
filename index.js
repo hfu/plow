3cf2ac93d795e8f710c3c49d64e0c610f9805431
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2021-01-12 22:34:54 +0900

    update

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const { chromium } = require('playwright');
   const page = await browser.newPage({
     viewport: { width: 512, height: 512 }
   })
-  await page.goto('https://hfu.github.io/plow/#18/32.7528/129.888')
-  await page.waitForNavigation()
+  await page.goto(`https://hfu.github.io/plow/#8/32.7528/129.888`)
   await page.waitForTimeout(1500)
-  await page.screenshot({ path: `img/a.png` })
+  for (let i = 10; i < 18; i++) {
+    await page.goto(`https://hfu.github.io/plow/#${i}/32.7528/129.888`)
+    await page.waitForNavigation()
+    //await page.waitForTimeout(500)
+    await page.screenshot({ path: `img/a${i}.png` })
+    console.log(i)
+  }
   await browser.close()
 })()
 
